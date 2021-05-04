bb0d872dd01df8544709ea55b68195fb3416bec4
Pool Patrik Oliveira <ppoliveira@secullum.local>
2021-05-04 09:10:06 -0300

    tratando exceptions de integridade referencial

--- SalesWeb3.1/Controllers/SellersController.cs
+++ SalesWeb3.1/Controllers/SellersController.cs
@@ -66,8 +66,16 @@ namespace SalesWeb31.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id) //parametro opcional
         {
-            _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
--- SalesWeb3.1/Services/SellerService.cs
+++ SalesWeb3.1/Services/SellerService.cs
@@ -38,9 +38,16 @@ namespace SalesWeb31.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
 
         }
 
